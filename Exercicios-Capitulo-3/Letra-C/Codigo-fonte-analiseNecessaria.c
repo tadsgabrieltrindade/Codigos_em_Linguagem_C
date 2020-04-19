68c426f12ede45decb2abe7dcc55677c42e332ba
Gabriel Trindade <icandoitgrt@gmail.com>
2020-04-19 13:23:25 -0300

    add letra c

--- Exercicios-Capitulo-3/Letra-C/Codigo-fonte-analiseNecessaria.c
+++ Exercicios-Capitulo-3/Letra-C/Codigo-fonte-analiseNecessaria.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<locale.h>
+#include<math.h>
 
 int main(){
     setlocale(LC_ALL, "Portuguese");
     float altura, raio, volume, area;
-    printf("Digite o raio da base: \n");
+    printf("Digite o raio da base: ");
     scanf("%f", &raio);
         area=3,14159*raio*raio;
-    printf("\n A área da base é de %.1f metros quadrados.", area);
-    printf("\n Digite a altura da do cilindro: \n");
+    printf("\n A área da base é de %.1f metros quadrados.\n", area);
+    printf("Digite a altura da do cilindro: ");
     scanf("%f", &altura);
         volume=area*altura;
     printf("\n O volume do cilindro é de %.1f metros cúbicos.", volume);
---
+++ Exercicios-Capitulo-3/Letra-D/README.md
@@ -0,0 +1,1 @@
+# Efetuar o cálculo da quantidade de litros de combustível gasta em uma viagem,
