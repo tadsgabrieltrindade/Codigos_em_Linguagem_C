6147a68df459850f1cafeea23a10ac602c2f5225
Gabriel Trindade <icandoitgrt@gmail.com>
2020-04-19 21:23:50 -0300

    add do C letra D

--- Exercicios-Capitulo-3/Letra-A/Código-Fonte.c
+++ Exercicios-Capitulo-3/Letra-A/Código-Fonte.c
@@ -5,11 +5,11 @@
 int main(){
     setlocale(LC_ALL, "Portuguese");
     float Tc, F;
-    printf("\t\t\t\t Conversor de temperatura de Celsius para Fahrenheit");
-    printf("\n Digite a temperatura em Celsius: \n");
+    printf("\t\t\t\t LETRA A \n");
+    printf("\n Digite a temperatura em Celsius: ");
     scanf("%f", &Tc);
     F = ((9*Tc+160)/5);
-    printf("\n Sua temperatura é %.1f°F \n", F);
+    printf(" \n Sua temperatura é %.1f°F \n", F);
     system("pause");
 
 return 0;
--- Exercicios-Capitulo-3/Letra-D/Estrutura.c
+++ Exercicios-Capitulo-3/Letra-D/Estrutura.c
@@ -5,7 +5,16 @@
 
 int main(){
     setlocale(LC_ALL, "Portuguese");
+float TEMPO, VELOCIDADE, DISTANCIA, LITROS_USADOS;
+        printf("\t\t\t\t\t LETRA D \n\n");
+    printf("Digite o tempo gasto da viagem em minutos: ");
+    scanf("%f", &TEMPO);
+    printf("Digite a velocidade média da viagem: ");
+    scanf("%f", &VELOCIDADE);
+DISTANCIA = TEMPO * VELOCIDADE;
+LITROS_USADOS = DISTANCIA / 12;
 
+printf(" \n A velocidade média foi de %.1f Km/h ;\n O tempo percorrido da viagem for de %.1f Min; \n A distância percorrida foi de %.1f Km e; \n A quantidade de litros usado durante a viagem foi de %.1f Litros.\n", VELOCIDADE, TEMPO, DISTANCIA, LITROS_USADOS);
 
     system("\n pause");
 return 0;
