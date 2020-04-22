0461a3ba6f476538345cbd02e8429fda49bfff81
Gabriel Trindade <icandoitgrt@gmail.com>
2020-04-22 15:49:35 -0300

    up

--- Exercicios-Capitulo-3/Letra-C/Codigo-fonte-analiseNecessaria.c
+++ Exercicios-Capitulo-3/Letra-C/Codigo-fonte-analiseNecessaria.c
@@ -5,15 +5,19 @@
 
 int main(){
     setlocale(LC_ALL, "Portuguese");
-    float altura, raio, volume, area;
-    printf("Digite o raio da base: ");
+    float altura, raio, volume, area, pi;
+
+    altura = raio = volume = area = 0;
+      pi = 3,14159;
+    printf("Digite o raio da base em cm: ");
     scanf("%f", &raio);
-        area=3,14159*raio*raio;
-    printf("\n A Ã¡rea da base Ã© de %.1f metros quadrados.\n", area);
+        area = (pi*( pow(raio, 2)));
+    printf("\n A área da base é de %1.f centímetros quadrados.\n", area);
     printf("Digite a altura da do cilindro: ");
     scanf("%f", &altura);
-        volume=area*altura;
-    printf("\n O volume do cilindro Ã© de %.1f metros cÃºbicos.", volume);
+        volume = (area*altura);
+
+    printf("\n O volume do cilindro é de %1.f centímetros cúbicos.", volume);
 return 0;
 system("\npause");
 }
