7e225219dfcae4394f3caa2206aefcc9b44fa112
Gabriel Trindade <icandoitgrt@gmail.com>
2020-04-26 19:59:11 -0300

    Letra E adicionada

--- Manzano/Exercicios-Capitulo-3/Letra-E/Código-fonte.c
+++ Manzano/Exercicios-Capitulo-3/Letra-E/Código-fonte.c
@@ -1,12 +1,27 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include<ctype.h>
 #include<locale.h>
-#include<math.h>
 
-int main(){
-    setlocale(LC_ALL, "Portuguese");
+int main()
+{
+    setlocale(LC_ALL,"Portuguese");
+
+float V, TX, TMP, PST;
+//V = TX = TMP = PST = 0.0;
+printf("\t\t\t\tPrestação em atraso");
+
+printf("\n\nDigite o valor da prestação: \n>>> ");
+scanf("%f", &V);
+printf("\nDigite a taxa da prestação: \n>>> ");
+scanf("%f", &TX);
+printf("\nDigite o tempo da prestação: \n>>> ");
+scanf("%f", &TMP);
+
+PST = (V+(V*(TX/100)*TMP));
+
+printf("\nO valor da prestação em atraso é de R$%1.f", PST);
 
 
-    system("\n pause");
-return 0;
+    return 0;
 }
