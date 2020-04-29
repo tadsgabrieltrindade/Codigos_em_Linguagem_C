f2dd949442a3a54b250217c7eb98c7dac9da23db
Gabriel Trindade <icandoitgrt@gmail.com>
2020-04-29 15:48:08 -0300

    Letra R

---
+++ Manzano/Exercicios-Capitulo-3/Letra-R/Codigo-fonte.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<ctype.h>
+#include<locale.h>
+#include<math.h>
+
+int main()
+{
+    setlocale(LC_ALL,"Portuguese");
+int candA, candB, candC, votosNulos, votosBrancos, votosValidos, qntEleitores;
+candA = candB = candC = votosNulos = votosBrancos = votosValidos = qntEleitores = 0;
+float percentualValidos, percentualA, percentualB, percentualC, percentualNulos, percentualBrancos;
+percentualValidos = percentualA = percentualB = percentualC = percentualNulos = percentualBrancos = 0.0;
+printf("Digite a quantidade de votos do candidato A: \n>>> ");
+scanf("%i", &candA);
+printf("\nDigite a quantidade de votos do candidato B: \n>>> ");
+scanf("%i", &candB);
+printf("\nDigite a quantidade de votos do candidato C: \n>>> ");
+scanf("%i", &candC);
+printf("\nDigite a quantidada de votos nulos: \n>>> ");
+scanf("%i", &votosNulos);
+printf("\nDigite a quantidade de votos em branco: \n>>> ");
+scanf("%i", &votosBrancos);
+
+votosValidos = (candA + candB + candC);
+qntEleitores = votosValidos + votosNulos + votosBrancos;
+percentualValidos = ((votosValidos * 100) / qntEleitores);
+percentualA = ((candA * 100) / qntEleitores);
+percentualB = ((candB * 100) / qntEleitores);
+percentualC = ((candC * 100) / qntEleitores);
+percentualNulos = ((votosNulos * 100) / qntEleitores);
+percentualBrancos = ((votosBrancos * 100) / qntEleitores);
+
+printf("\nA quantidade de eleitores foi de %i.\n", qntEleitores);
+printf("\nO percentual de votos válidos em relação ao total foi de %1.f%%.\n", percentualValidos);
+printf("\nO percentual de votos válidos do candidato A em relação ao total foi de %1.f%%.\n", percentualA);
+printf("\nO percentual de votos válidos do candidato B em relação ao total foi de %1.f%%.\n", percentualB);
+printf("\nO percentual de votos válidos do candidato C em relação ao total foi de %1.f%%.\n", percentualC);
+printf("\nO percentual de votos nulos em relação ao total foi de %1.f%%.\n", percentualNulos);
+printf("\nO percentual de votos em branco em relação ao total foi de %1.f%%.\n", percentualBrancos);
+
+
+
+
+
+
+    system("\n\npause\n");
+    return 0;
+}
