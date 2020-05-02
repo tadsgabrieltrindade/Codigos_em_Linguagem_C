dbd201833983163be43ca0d2b188d43ef1813b0c
Gabriel Trindade <62914562+tadsgabrieltrindade@users.noreply.github.com>
2020-05-02 16:27:12 -0300

    Update Codigo-fonte.c

--- Manzano/Atividades-Capitulo-4/Letra-A/Codigo-fonte.c
+++ Manzano/Atividades-Capitulo-4/Letra-A/Codigo-fonte.c
@@ -9,7 +9,7 @@ int main()
     setlocale(LC_ALL,"Portuguese");
 int A, B, R;
 
-printf("\t\t\t\tCapítulo 4 - Letra Z\n\n");
+printf("\t\t\t\tCapÃ­tulo 4 - Letra A\n\n");
 printf("Digite uma valor: \n>>> ");
 scanf("%i", &A);
 printf("\nDigite outro valor: \n>>> ");
@@ -17,10 +17,10 @@ scanf("%i", &B);
 
     if (A > B){
         R = (A - B);
-        printf("\nO resultada da diferênça entre %i e %i é %i. \n", A, B, R);
+        printf("\nO resultada da diferÃªnÃ§a entre %i e %i Ã© %i. \n", A, B, R);
     }else{
         R = (B - A);
-         printf("\nO resultada da diferênça entre %i e %i é %i. \n", B, A, R);
+         printf("\nO resultada da diferÃªnÃ§a entre %i e %i Ã© %i. \n", B, A, R);
     }
 
 system("\n\npause\n");
