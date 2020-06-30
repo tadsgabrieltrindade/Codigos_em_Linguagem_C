aa787dd10d50d53723d6dd03b70aecec86fe4855
Gabriel Trindade <icandoitgrt@gmail.com>
2020-06-29 22:02:47 -0300

    add

--- Projetos de estudo/IDE para testa códigos em C/main.c
+++ Projetos de estudo/IDE para testa códigos em C/main.c
@@ -5,20 +5,24 @@
 #include <math.h>
 
 int main (){
-    printf("\t\t\t\tLetra K - Cap. 5");
-    setlocale(LC_ALL, "Portuguese");
-float soma, pot;
-int i;
-i=soma=pot=0;
 
-do {
-    pot = pow(2,i);
-    soma = pot + soma;
-    i++;
-}while(i<=64);
+printf("\t\t\t\tLetra L - Cap. 5");
+setlocale(LC_ALL, "Portuguese");
 
-printf("\nO somatório dos grãos de trigo é %17.f.\n", soma);
-
+int valorLido, i, valorGuardado, fat, somaTotal, cont;
+valorLido = valorGuardado = 0; fat = i = somaTotal = cont=1;
+printf("\nDigite 15 números inteiros: \n");
+do{
+        printf("\n%iº >>> ", i);
+        scanf("%i", &valorLido);
+            if(cont <= valorLido){
+                fat = fat * cont;
+                cont++;
+            }
+        somaTotal = fat + somaTotal;
+   i++;
+}while(i<=15);
+printf("\nO valor do somatório do fatorial dos 15 números inseridos é %i.\n", somaTotal);
 system("\npause\n");
 return 0;
 }
