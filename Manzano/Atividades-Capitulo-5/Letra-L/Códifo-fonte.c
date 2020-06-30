f483a5a6e21656b652db3cd6270d85dacdfd2af0
Gabriel Trindade <icandoitgrt@gmail.com>
2020-06-30 16:00:39 -0300

    Complete Letra L

--- Manzano/Atividades-Capitulo-5/Letra-L/Códifo-fonte.c
+++ Manzano/Atividades-Capitulo-5/Letra-L/Códifo-fonte.c
@@ -9,8 +9,8 @@ int main (){
 printf("\t\t\t\tLetra L - Cap. 5");
 setlocale(LC_ALL, "Portuguese");
 
-int valorLido, i, fat, somaTotal, cont;
-valorLido  = somaTotal = 0; fat = i = cont=1;
+int valorLido, i, fat, total, cont;
+valorLido  = total = 0; fat = i = cont=1;
 printf("\nDigite 15 números inteiros: \n");
 
 do{
@@ -19,11 +19,11 @@ do{
 
             for (cont = 1, fat = 1; cont <= valorLido; cont++)
                 fat = fat * cont;
-        somaTotal = fat + somaTotal;
+        total = fat + total;
    i++;
 }while(i<=15);
 
-printf("\nO valor do somatório do fatorial dos 15 números inseridos é %i.\n", somaTotal);
+printf("\nO valor do somatório do fatorial dos 15 números inseridos é %i.\n", total);
 system("\npause\n");
 return 0;
 }
