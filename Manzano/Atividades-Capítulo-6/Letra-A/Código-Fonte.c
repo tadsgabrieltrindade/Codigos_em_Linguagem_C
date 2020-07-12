116b8dbdfd00860877a918daec482f8817e16e1f
Gabriel Trindade <icandoitgrt@gmail.com>
2020-07-11 21:16:29 -0300

    Add Letra A - Cap.6

--- Manzano/Atividades-Capítulo-6/Letra-A/Código-Fonte.c
+++ Manzano/Atividades-Capítulo-6/Letra-A/Código-Fonte.c
@@ -4,18 +4,24 @@
 #include <ctype.h>
 #include <math.h>
 
-int main()
-{
-    setlocale(LC_ALL,"Portuguese");
-int i, R;
-printf("\t\t\t\tCapítulo 5 - Letra A\n\n");
-i = 15;
-while((i >= 15) && (i <= 200)){
-        R = i * i;
-        printf("%i.\n", i, R);
-        i++;
-    }
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+    printf("\t\t\t\Capítulo 6 - Letra A\n\n");
 
- system("\npause\n");
+    char pessoa[11][15]; int i = 1;
+    printf("\nDigite os primeiros nomes das pessoas dez vezes: \n");
+do{
+    printf("%iº >>> ", i);
+    scanf("%s", &pessoa[i]);
+    i++;
+}while(i<=10);
+    printf("\n");
+
+    for(int linha = 1; linha<=50; linha++){
+            printf("-");}
+            printf("\nOs nomes das pessoas inseridas são: ");
+    for (i = 1; i<=10; i++){
+        printf("\n%iº pessoa: %s ", i, pessoa[i]);}
+    printf("\n");
     return 0;
 }
--- Área-de-testes/IDE para testa códigos em C/main.c
+++ Área-de-testes/IDE para testa códigos em C/main.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 #include <ctype.h>
+#include <math.h>
 
 int main() {
-  char nome [15]; char sobrenome [20]; int i = 0; int sep;
-  do{
-  printf("\nDigite seu nome: ");
-  scanf("%s", &nome[i]);
-  printf("\nAgora digite seu sobrenome: ");
-  scanf("%s", &sobrenome[i]);
-  i++;
-  for (sep = 1; sep<=60; sep++)
-  printf("-");
-  }while(i<=2);
-  printf("\nOs nomes apresentados são: \n");
-  for (i = 0; i <=2; i++){
-    printf("\n%iº nome: %s %s", i, nome[i], sobrenome[i]);
-    i++;
-  }
+    setlocale(LC_ALL, "Portuguese");
+    printf("\t\t\t\Capítulo 6 - Letra A\n\n");
 
+    char pessoa[11][15]; int i = 1;
+    printf("\nDigite os primeiros nomes das pessoas dez vezes: \n");
+do{
+    printf("%iº >>> ", i);
+    scanf("%s", &pessoa[i]);
+    i++;
+}while(i<=10);
+    printf("\n");
+
+    for(int linha = 1; linha<=50; linha++){
+            printf("-");}
+            printf("\nOs nomes das pessoas inseridas são: ");
+    for (i = 1; i<=10; i++){
+        printf("\n%iº pessoa: %s ", i, pessoa[i]);}
+    printf("\n");
     return 0;
 }
