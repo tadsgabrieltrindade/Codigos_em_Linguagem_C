e497d0c90e3ffb8f8fc44489761e20a9c2a32dcc
Gabriel Trindade <icandoitgrt@gmail.com>
2020-07-15 09:50:44 -0300

    add letra B do capítulo 6

---
+++ Atividades do Manzano/Atividades do capítulo 6/README.md
@@ -0,0 +1,3 @@
+# Sobre os bloco de notas ...
+
+Como o conteúdo de array é novo para mim neste momento, não conseguir compreender ainda como construir diagrama de blocos com este tipo de variável - ARRAY. Quando possível, irei colocar os diagramas de blocos conforme cada atividade proposta.
--- Área-de-testes/IDE para testa códigos em C/main.c
+++ Área-de-testes/IDE para testa códigos em C/main.c
@@ -8,19 +8,18 @@ int main() {
     setlocale(LC_ALL, "Portuguese");
     printf("\t\t\tCapítulo 6 - Letra B\n\n");
 
-    int A [8] [8]; int B [8] [8] ; int i = 1;
+    int arrayA[] = {0,1,2,3,4,5,6,7}; int arrayB[] = {0,1,2,3,4,5,6,7}; int i = 0;
     printf("\nDigite oito valores para a matriz A: \n");
     do{
-        printf(">>> "); scanf("%i",  &A[i]);
+        printf(">>> "); scanf("%i",  &arrayA[i]);
         i++;
-    }while(i<=8);
-
+    }while(i<8);
      printf("\nOs números inseridos na matriz A, passando-os para a matriz B, ficam: \n");
-    for(i = 0; i<=8; i++){
-        *B[i] = (*A[i]) * 3;
-        printf("\nPara posição %i da matiz A temos %i na matriz B ", i, B[i]);
+    for(i = 0; i<8; i++){
+        arrayB[i] = arrayA[i] * 3;
+        printf("\n>> Para posição %i da matiz A que vale %i, temos %i na matriz B ", i, arrayA[i], arrayB[i]);
     }
-    printf("\n");
+    printf("\n\n");
     system("\npause");
     return 0;
 }
