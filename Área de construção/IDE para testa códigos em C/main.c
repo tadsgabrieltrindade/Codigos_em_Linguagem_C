f98fe444102ddc4f46919510992805fb39703255
Gabriel Trindade <icandoitgrt@gmail.com>
2020-09-05 18:22:50 -0300

    up2

--- Área de construção/IDE para testa códigos em C/main.c
+++ Área de construção/IDE para testa códigos em C/main.c
@@ -3,34 +3,62 @@
 #include<locale.h>
 #include<ctype.h>
 
-int main (){
-    setlocale(LC_ALL, "Portuguese");
-    printf("\t\t\t\tCapítulo 4 - Letra E\n\n");
+int main(){
 
-float A, B, C, delta, x1, x2;
-A = B = C = delta = x1 = x2 = 0.0;
-printf("Digite o coeficiente A: \n>>> ");
-scanf("%f", &A);
-printf("\nDigite o coeficiente B: \n>>> ");
-scanf("%f", &B);
-printf("\nDigite o coeficiente C: \n>>> ");
-scanf("%f", &C);
-delta = ((pow(B, 2)) - (4 * A * C));
-    if (delta < 0){
-        printf("\nSeu delta é negativo, logo não tem raiz real.\n");
-    }else{
-        if (delta == 0){
-            x1 = (-(B) / (2*A));
-            printf("\nSeu delta é igual a zero, logo ele tem uma raiz real: x1=%.2f.\n", x1);
-        }else{
-            x1 = (((-(B)) + (pow(delta, 0.5))) / (2 * A));
-            x2 = (((-(B)) - (pow(delta, 0.5))) / (2 * A));
-            printf("\nSeu delta é maior que zero, logo tem duas raízes reais: x1=%.2f e x2=%.2f.\n", x1,x2);
-        }
+    int A, B, C; int i = 1;
+do{
+    system("cls");
+    printf("\nDigite um valor para A: \n>>>");
+    scanf("%i", &A);
+    printf("\nDigite um valor para B: \n>>>");
+    scanf("%i", &B);
+    printf("\nDigite um valor para C: \n>>>");
+    scanf("%i", &C);
+    if (A<B && A<C){
+        if (B<C)
+            printf("\nA ordem crescente é: %i - %i - %i\n", A,B,C);
+            else
+            printf("\nA ordem crescente é: %i - %i - %i\n", A,C,B);
+    }
+    if (B<A && B<C){
+        if (A<C)
+            printf("\nA ordem crescente é: %i - %i - %i\n", B,A,C);
+            else
+            printf("\nA ordem crescente é: %i - %i - %i\n", B,C,A);
+    }
+    if (C<A && C<B){
+        if (A<B)
+            printf("\nA ordem crescente é: %i - %i - %i\n", C,A,B);
+            else
+            printf("\nA ordem crescente é: %i - %i - %i\n", C,B,A);
     }
 
+     if (A == B){
+        if (B==C)
+            printf("\nNúmeros iguais!\n");
+            else
+                if (C<A)
+                    printf("\nA ordem crescente é: %i - %i \n", C, A);
+                else
+                     printf("\nA ordem crescente é: %i - %i \n", A, C);
+    }
 
-        system("\npause\n");
-        return 0;
+    if (B == C){
+        if (B<A)
+            printf("\nA ordem crescente é: %i - %i \n", B, A);
+        else
+            printf("\nA ordem crescente é: %i - %i \n", A, B);
+    }
 
+    if (A == C){
+        if (A<B)
+            printf("\nA ordem crescente é: %i - %i \n", A, B);
+        else
+            printf("\nA ordem crescente é: %i - %i \n", B, A);
+    }
+    printf("\nDigite i: ");
+    scanf("%i", &i);
+}while(i==1);
+    system("\npause");
+    return 0;
 }
